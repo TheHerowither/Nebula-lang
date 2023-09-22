6f132d5a13951d37a1dfd41e63bba22b874b9d4a
TheHerowither <104439145+TheHerowither@users.noreply.github.com>
2023-09-22 14:09:27 +0200

    Modified build naming

--- buildasm.bat
+++ buildasm.bat
@@ -1,10 +1,12 @@
 @echo off
 set file=%~1
+set name=%~2
 ::mkdir build >> build\build.log
 del build\build.log
 ::echo Building %file%
-nasm -fwin32 %file% -o build/main.obj
-GoLink /console /entry _start build/main.obj kernel32.dll user32.dll gdi32.dll -o build\main.exe >> build\build.log
+mkdir build\%name% >> build\build.log
+nasm -fwin32 %file% -o build/%name%/%name%.obj
+GoLink /console /entry _start build/%name%/%name%.obj kernel32.dll user32.dll gdi32.dll -o build\main.exe >> build\build.log
 echo Running your program
-call "build\main.exe"
+call "build\%name%\%name%.exe"
 echo Your program finished with exit code %errorlevel%
--- main.py
+++ main.py
@@ -14,19 +14,22 @@ except Exception as e:
     exit(1)
 
 if path != "--version":
+    name = path.split("\\")[len(path.split("\\")) - 1]
+    name = name.split("/")[len(name.split("/")) - 1]
+    name = name.split(".")[0]
     file = open(path)
     if not os.path.exists("build"):
         os.mkdir("build")
     print(f"Building {sys.argv[1]}")
-    with open("build\\main.asm", "w") as f:
+    with open(f"build\\{name}.asm", "w") as f:
         tokens = tokenize(file.read())
         #print(*tokens)
         asm = tokens_to_asm(tokens)
         f.write(asm)
 
     print()
-    os.system("call buildasm.bat build\\main.asm")
-    print(f"Executable uses {os.stat('./build/main.exe').st_size} bytes")
+    os.system(f"call buildasm.bat build\\{name}.asm {name}")
+    print(f"Executable uses {os.stat(f'./build/{name}/{name}.exe').st_size} bytes")
 else:
     print(f"Nebula version {VERSION}")
     print(f"Built on {BUILD_DATE}")
